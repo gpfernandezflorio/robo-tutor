d3b5b0c7fab5ac264ca94fc33eaa1fedf961ed35
Gonzalo Pablo Fernandez <gpfernandez@dc.uba.ar>
2024-03-09 17:38:39 -0300

    Agrego argumentos a main.py

--- main.py
+++ main.py
@@ -26,24 +26,45 @@ def obtener_pid(s):
       return x
   return None
 
-def kill_previous():
+def a_eliminar(pids, s):
+  for p in pids:
+    if s.endswith(py + " server.py -p80" + str(50+p)) or s.endswith(otropy + " server.py -p80" + str(50+p)):
+      return True
+  return False
+
+def kill_previous(pids):
   from subprocess import PIPE, Popen
   p = Popen('ps -x', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
   out = p.communicate()[0]
-  print(out)
+  # print(out)
   for s in out.split('\n'):
-    if (py + " server.py -p80" in s) or (otropy + " server.py -p80" in s):
+    if a_eliminar(pids, s):
       pid = obtener_pid(s)
       if not (pid is None):
         if int(pid) != os.getpid():
           print("kill " + pid)
           os.system("kill -9 " + pid)
 
-async def main():
+async def main(i):
   process = await asyncio.create_subprocess_exec(py, 'server.py', '-p80' + str(50+i))
   print(f'subprocess: {process}')
 
-kill_previous()
+pids = []
+if len(sys.argv) > 1:
+  for x in sys.argv:
+    try:
+      p = int(x)
+      pids.append(p)
+    except ValueError:
+      continue
+if len(pids) == 0:
+  pids = [0,1,2,3,4,5,6,7,8,9,10]
+
+kill_previous(pids)
+
+if len(sys.argv) > 1 and 'x' in sys.argv:
+  exit(0)
+
 os.chdir('servidor')
-for i in range(11):
-  asyncio.run(main())
+for i in pids:
+  asyncio.run(main(i))
